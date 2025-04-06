b45df29f16451f411de80ff4570ca738d08fdb81
Miguel <misterbryn99@gmail.com>
2025-04-06 19:55:40 +0200

    Añadido miniatura

--- scripts/script-header.js
+++ scripts/script-header.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var h1 = document.querySelector('h1');
     if (h1) {
-        h1.textContent = 'Titulo modificado';
+        h1.textContent = 'Principales Noticias';  //Titulo personalizado
     }
 });
